5ce0155eec2d8b9ef044f78fc60b2c23b3511906
Dembel02 <IRGilyazov@yandex.ru>
2025-02-26 23:43:51 +0500

    1

--- Selenium_1.py
+++ Selenium_1.py
@@ -1,6 +1,9 @@
 import time
 from selenium import webdriver
+from selenium.webdriver.chrome.service import Service
 from webdriver_manager.chrome import ChromeDriverManager
+
+service = Service(ChromeDriverManager().install())
 
 
 with webdriver.Chrome(ChromeDriverManager().install()) as browser:
@@ -9,12 +12,12 @@ with webdriver.Chrome(ChromeDriverManager().install()) as browser:
 
 #Или 
 
-import time
-from selenium import webdriver
-from webdriver_manager.chrome import ChromeDriverManager
-from webdriver_manager.core.os_manager import ChromeType
-from selenium.webdriver.chrome.service import Service as ChromiumService
+# import time
+# from selenium import webdriver
+# from webdriver_manager.chrome import ChromeDriverManager
+# from webdriver_manager.core.os_manager import ChromeType
+# from selenium.webdriver.chrome.service import Service as ChromiumService
 
-with webdriver.Chrome(service=ChromiumService(ChromeDriverManager().install())) as browser:
-    browser.get("https://stepik.org/course/104774")
-    time.sleep(5)
+# with webdriver.Chrome(service=ChromiumService(ChromeDriverManager().install())) as browser:
+#     browser.get("https://stepik.org/course/104774")
+#     time.sleep(5)
